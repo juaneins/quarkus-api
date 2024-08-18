cd65c0fb52b8d9bbaf101a3516ba3b84c2139d45
juaneins_uio <ballesterosjg@gmail.com>
2024-08-18 17:58:01 -0500

    usar StreamConfiguration para obtener entidades relacionadas

--- src/main/java/org/freecodecamp/app/FilmResource.java
+++ src/main/java/org/freecodecamp/app/FilmResource.java
@@ -42,5 +42,19 @@ public class FilmResource {
     		   .collect(Collectors.joining("\n"));
     }
     
+    @GET
+    @Path("/actors/{startsWith}/{minLength}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String actors(String startsWith, short minLength) {
+       return filmRepository.actors(startsWith, minLength)
+    		   .map(f -> String.format("%s (%d min): %s", 
+    				   f.getTitle(), 
+    				   f.getLength(), 
+    				   f.getActors().stream()
+    				   		.map(a -> String.format("%s %s ", a.getFirstName(), a.getLastName()))
+    				   		.collect(Collectors.joining(","))))
+    		   .collect(Collectors.joining("\n"));
+    }
+    
     
 }
--- src/main/java/org/freecodecamp/app/repository/FilmRepository.java
+++ src/main/java/org/freecodecamp/app/repository/FilmRepository.java
@@ -3,10 +3,12 @@ package org.freecodecamp.app.repository;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import org.freecodecamp.app.model.Actor;
 import org.freecodecamp.app.model.Film;
 
 import com.speedment.jpastreamer.application.JPAStreamer;
 import com.speedment.jpastreamer.projection.Projection;
+import com.speedment.jpastreamer.streamconfiguration.StreamConfiguration;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -34,6 +36,14 @@ public class FilmRepository {
 				.sorted(Film$.length)
 				.skip(page * PAGE_SIZE)
 				.limit(PAGE_SIZE);
+	}
+	
+	public Stream<Film> actors(String startsWith, short minLength) {
+		final StreamConfiguration<Film> sc = StreamConfiguration.of(Film.class).joining(Film$.actors);
+		return jpaStreamer.stream(sc)
+				.filter(Film$.title.startsWith(startsWith).and(Film$.length.greaterThan(minLength)))
+				.sorted(Film$.length.reversed());
+		
 	}
 
 }
